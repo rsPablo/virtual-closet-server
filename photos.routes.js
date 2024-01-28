4b544aafe0de25a6f122e2d7dacac9050e00f5d1
pablo <pabrodsab.git@gmail.com>
2024-01-28 14:07:09 +0100

    feat: add aws sdk config

--- photos.routes.js
+++ photos.routes.js
@@ -1,11 +1,21 @@
 const { Router } = require("express");
+const { uploadFile, readFile } = require("./s3");
 const router = Router();
 
 router.get("/", (req, res) => res.send("welcome to server"));
 
-router.post("/upload", (req, res) => {
-  console.log(req.files);
+router.post("/upload", async (req, res) => {
+  console.log(req.files["photo"].tempFilePath);
+
+  const result = await uploadFile(req.files["photo"]);
+  console.log(result);
   res.send("archivo subido");
+});
+
+router.get("/archivo", async (req, res) => {
+  const result = await readFile();
+  console.log(result);
+  res.send("tu archivo");
 });
 
 module.exports = router;
